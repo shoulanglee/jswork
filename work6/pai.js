0ccca2c4b9a457bbf511dc4181e80df18ecc8c21
shoulanglee <2846921943@qq.com>
2019-10-15 18:32:39 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -2,22 +2,35 @@ function calculate(){
     let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('').value =pi()
+        document.getElementById('pai').value = pi(num)
     }
 }
+
 function factorial(n){
     let sum = 1
-    for(let i=++i){
-        
+    for(let i=1;i<=n;++i){
+        sum *=i
     }
     return sum
 }
-function pai(num){
-    let sum = 0
-    for(let i=0;i<=num;++i){
-        let dividend = factorial()
-        let divisor = oddFactorial()
+
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++i){
+        sum *= ((2*i)+1)
     }
-    return
+    return sum
 }
-calculate
+
+function pi(num) {
+    let sum =0
+    for (let i =0;i<=num;++i){
+        let  dividend = factorial(i)
+        let divisor = oddFactorial(i)
+        sum += (dividend/divisor)
+        console.log(sum)
+    }
+    // return Math.PI
+    return sum*2
+}
+calculate()
