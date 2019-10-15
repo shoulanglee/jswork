1aa22b3cbf0b6ba5fc9ebb93f7037282785553d0
shoulanglee <2846921943@qq.com>
2019-10-15 15:40:10 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,23 @@
+function calculate(){
+    let num = document.getElementById('num').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('').value =pi()
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=++i){
+        
+    }
+    return sum
+}
+function pai(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial()
+    }
+    return
+}
+calculate
