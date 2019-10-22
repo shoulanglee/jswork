1fa86e976c04801cabb28c97e49b5e8bbe52e4ec
shoulanglee <2846921943@qq.com>
2019-10-22 18:18:19 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,34 +1,30 @@
 function goldbach() {
-    var odd = document.getElementById('odd').value
-    console.log(odd)
-    if(odd%2!==0){
-        alert("请输入偶数")
+    var value = document.getElementById('odd').value;
+    if (value%2!== 0) {
+       alert("请输入一个大于2的偶数");
     }
-    let goldbach = document.getElementById('goldbach')
-    var str =[]
-    var a = 0;
-    for(var i=2;i<=odd;i++){
-        a = 0
-        for(var j=2;j<i;j++){
-            if(i%j==0){
-                a++
-            }
-        }
-        if(a==0){
-            str.push(i)
-        }
-    }
-    var str1 = ''
-    console.log(odd)
-    for(let i=0;i<(str.length)/2;i++){
-        console.log(111)
-       for(let j=0;j<str.length;j++){
-           if((str[i]+str[j])===Number(odd)){
-               str1 += odd+"可以拆分为两个质数"+str[i]+"与"+str[j]+"的和\n"
-            
-           }
+    let goldbach = document.getElementById('goldbach');
+    var arr = [];
+    for (let i= 2; i <= value; i++) {
+       let  a = 0
+       for (let j = 2; j < i; j++) {
+          if (i % j == 0) {
+             a++
+          }
+       }
+       if (a == 0) {
+          arr.push(i);
        }
     }
-    goldbach.innerText = str1
-   
-}
+ 
+ 
+    var str = '';
+    for (let i = 0; i < (arr.length) / 2; i++) {
+       for (let j = 0; j < arr.length; j++) {
+          if ((arr[i] + arr[j])== Number(value)) {
+             str +='<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+'</div>'
+          }
+       }
+    }
+    goldbach.innerHTML = str;
+ }
