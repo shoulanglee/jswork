c92fac2255a2964b1de092fa8ce2a76c598963e2
shoulanglee <2846921943@qq.com>
2019-10-22 18:28:39 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,30 +1,30 @@
 function goldbach() {
-    var value = document.getElementById('odd').value;
-    if (value%2!== 0) {
-       alert("请输入一个大于2的偶数");
-    }
-    let goldbach = document.getElementById('goldbach');
-    var arr = [];
-    for (let i= 2; i <= value; i++) {
-       let  a = 0
-       for (let j = 2; j < i; j++) {
-          if (i % j == 0) {
-             a++
-          }
-       }
-       if (a == 0) {
-          arr.push(i);
-       }
-    }
- 
- 
-    var str = '';
-    for (let i = 0; i < (arr.length) / 2; i++) {
-       for (let j = 0; j < arr.length; j++) {
-          if ((arr[i] + arr[j])== Number(value)) {
-             str +='<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+'</div>'
-          }
-       }
-    }
-    goldbach.innerHTML = str;
- }
+   var value = document.getElementById('odd').value;
+   if (value%2!== 0) {
+      alert("请输入一个大于2的偶数");
+   }
+   let goldbach = document.getElementById('goldbach');
+   var arr = [];
+   for (let i= 2; i <= value; i++) {
+      let  a = 0
+      for (let j = 2; j < i; j++) {
+         if (i % j == 0) {
+            a++
+         }
+      }
+      if (a == 0) {
+         arr.push(i);
+      }
+   }
+
+
+   var str = '';
+   for (let i = 0; i < (arr.length) / 2; i++) {
+      for (let j = 0; j < arr.length; j++) {
+         if ((arr[i] + arr[j])== Number(value)) {
+            str +='<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+'</div>'
+         }
+      }
+   }
+   goldbach.innerHTML = str;
+}
