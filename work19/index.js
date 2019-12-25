ef050b0e5407c95077a3215cf68b807bca3e42f3
shoulanglee <2846921943@qq.com>
2019-12-25 21:01:06 +0800

    work19

--- work19/index.js
+++ work19/index.js
@@ -9,21 +9,27 @@ function connect() {
         if (nickname) {
             ws.send('nickname|'+nickname)
         }
+        
     }
     ws.onclose = function() {
         document.getElementById('conn').disable='';
-        document.getElementById('disconn').disable= 'disabled';
+        document.getElementById('disconn').disable= 'disable';
     }
     ws.onmessage = function(event) {
         let board = document.getElementById('board')
         let newmsg = document.createElement('div')
         console.log(event.data)
         newmsg.innerHTML = event.data
+        board.appendChild(newmsg)
         board.scrollTop = board.scrollHeight;
     }
 }
 
 function disconnent() {
+    ws.close()
+}
+
+function send() {
     let msg = document.getElementById('content').value
     ws.send(msg)
 }
--- work19/index1.js
+++ work19/index1.js
@@ -3,7 +3,7 @@ document.getElementById('upload').onclick = function () {
     var fd = new FormData(form);
     var bar = document.getElementById('bar');
     var per = document.getElementById('per');
-    var dowm = document.getElementById('download');
+    var down = document.getElementById('download');
     var xhr = new XMLHttpRequest();
     xhr.upload.onprogress = function (e) {
         var num = Math.floor(e.loaded / e.total * 100);
@@ -12,16 +12,19 @@ document.getElementById('upload').onclick = function () {
     };
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status < 200 || xhr.status >= 300 && xhr.status !== 304){
+            if (xhr.status < 200 || xhr.status >= 300 && xhr.status !== 304) {
                 throw new Error('文件上传失败，服务器状态异常。');
             }
+            
             var name = xhr.responseText;
             if (name == '') {
                 throw new Error('服务器保存文件失败。');
             }
-            down.innerHTML = `文件上传成功。<a href=${name}>下载文件${name}</a>`;
+            let reg=/^http(s)?:\/\/(.*?)\//
+            let downurl = xhr.responseURL.match(reg)[0]+name.slice(2,name.length-1)
+            down.innerHTML = `文件上传成功。<a href=${downurl}>下载文件${downurl}</a>`;
         }
     };
-    xhr.open('POST','/upload');
+    xhr.open('POST', 'http://139.9.81.203:8090/upload');
     xhr.send(fd);
 };
